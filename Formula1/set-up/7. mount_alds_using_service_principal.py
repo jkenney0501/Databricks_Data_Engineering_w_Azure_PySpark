c2ad5e4931626948ceb1eb80d169cbc8ff7c7fb7
jkenney0501 <jkenney0501>
2023-07-15 23:32:01 +0000

    fix notebook classification error

--- Formula1/set-up/7. mount_alds_using_service_principal.py
+++ Formula1/set-up/7. mount_alds_using_service_principal.py
@@ -1,63 +1,68 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC ## Mount Azure Data Lake Containers for Project
+# MAGIC ## Mount Azure Data Lake using Service Principal
+# MAGIC 1. Get client_id, tenent_id and client_secret from key vault.
+# MAGIC 2. Call file system untility mount to mount the storage.
+# MAGIC 3. Explore other file system utilities related to mount.
 
 # COMMAND ----------
 
-# create a function to mount containers with names as arguements
-def mount_adls(storage_acct_name, container_name):
-    # get keys from vault
-    client_id = dbutils.secrets.get(scope='formula1-scope', key='f1-client-is')
-    tenant_id = dbutils.secrets.get(scope='formula1-scope', key='f1-tenant-id')
-    client_secret = dbutils.secrets.get(scope='formula1-scope', key='f1-client-secret')
-
-    # set spark config - see https://learn.microsoft.com/en-us/azure/databricks/dbfs/mounts
-    configs = {"fs.azure.account.auth.type": "OAuth",
-            "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-            "fs.azure.account.oauth2.client.id": client_id,
-            "fs.azure.account.oauth2.client.secret": client_secret,
-            "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
-    
-    # mount the container
-    dbutils.fs.mount(
-        source = f"abfss://{container_name}@{storage_acct_name}.dfs.core.windows.net/",
-        mount_point = f"/mnt/{storage_acct_name}/{container_name}",
-        extra_configs = configs)
-    
-    # show mounts on every run
-    display(dbutils.fs.mounts())
-  
-     
+# 1., 2. create variables to store client id and tenant id
+client_id = dbutils.secrets.get(scope='formula1-scope', key='f1-client-is')
+tenant_id = dbutils.secrets.get(scope='formula1-scope', key='f1-tenant-id')
+client_secret = dbutils.secrets.get(scope='formula1-scope', key='f1-client-secret')
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Mount Staging Container
+# MAGIC Replace
+# MAGIC
+# MAGIC "secret-scope" with the Databricks secret scope name. \
+# MAGIC "service-credential-key" with the name of the key containing the client secret. \
+# MAGIC "storage-account" with the name of the Azure storage account. \
+# MAGIC "application-id" with the Application (client) ID for the Azure Active Directory application. \
+# MAGIC "directory-id" with the Directory (tenant) ID for the Azure Active Directory application.
+# MAGIC
 
 # COMMAND ----------
 
-# pass in the storage account name and the container name
-mount_adls('dlformula1jk', 'stage')
+# set spark config - see https://learn.microsoft.com/en-us/azure/databricks/dbfs/mounts
+
+configs = {"fs.azure.account.auth.type": "OAuth",
+          "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+          "fs.azure.account.oauth2.client.id": client_id,
+          "fs.azure.account.oauth2.client.secret": client_secret,
+          "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
+
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ## Mount Clean Container
+# Mount - Optionally, you can add <directory-name> to the source URI of your mount point.
+dbutils.fs.mount(
+  source = "abfss://demo@dlformula1jk.dfs.core.windows.net/",
+  mount_point = "/mnt/dlformula1jk/demo",
+  extra_configs = configs)
 
 # COMMAND ----------
 
-mount_adls('dlformula1jk', 'clean')
+# use file system sematics now to list files in the demo container
+display(dbutils.fs.ls("/mnt/dlformula1jk/demo"))
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ## Mount Final Container
+# read the circuits file with spark dataframe api using file system sematics
+display(spark.read.csv("/mnt/dlformula1jk/demo/circuits.csv"))
 
 # COMMAND ----------
 
-mount_adls('dlformula1jk', 'final')
+# See all the mounts in a workspace
+display(dbutils.fs.mounts())
 
 # COMMAND ----------
 
 # to unmount - not going to run but just for demonstration purposes
 # dbutils.fs.unmount('/mnt/dlformula1jk/demo')
+
+# COMMAND ----------
+
+
