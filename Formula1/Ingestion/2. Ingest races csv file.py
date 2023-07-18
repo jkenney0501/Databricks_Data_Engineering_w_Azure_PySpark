f14a1a5d3c5ab2d528de485076f05867b42675a7
jkenney0501 <jkenney0501>
2023-07-18 22:24:00 +0000

    completed ingestion by ingesting races file and writing it adls as parquet. also partitoned by race year.

---
+++ Formula1/Ingestion/2. Ingest races csv file.py
@@ -0,0 +1,130 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC ## Ingest the races csv file:
+# MAGIC 1. Drop the url column
+# MAGIC 2. Transform-Rename raceId, year and circuitId to use snake case.
+# MAGIC 3. Transform-create a new column to concatenate date and time to create race_timestamp.
+# MAGIC 4. Add new column called ingestion_date using current timestamp.
+# MAGIC 5. Write file as parquet to sink.
+
+# COMMAND ----------
+
+# MAGIC %md See the files in the container
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/dlformula1jk/stage
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Create the schema**
+
+# COMMAND ----------
+
+# Import the required packages
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
+
+# COMMAND ----------
+
+races_schema = StructType(fields=[
+    StructField('raceId', IntegerType(), False),
+    StructField('year', IntegerType(), True),
+    StructField('round', IntegerType(), True),
+    StructField('circuitId', IntegerType(), True),
+    StructField('name', StringType(), True),
+    StructField('date', DateType(), True),
+    StructField('time', StringType(), True),
+    StructField('url', StringType(), True)
+])
+
+# COMMAND ----------
+
+# import the races.csv file. Infer the schema for now.
+races_df_stg = spark.read \
+    .option('header', True) \
+    .schema(races_schema) \
+    .csv('/mnt/dlformula1jk/stage/races.csv')
+
+# COMMAND ----------
+
+# see the schema 
+races_df_stg.printSchema()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Step 1** 
+# MAGIC - Drop the url column
+
+# COMMAND ----------
+
+# drop the url column since its not needed
+races_df_stg = races_df_stg.drop('url')
+
+display(races_df_stg)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Step 2**
+# MAGIC - Transform-Rename raceId, year and circuitId to use snake case.
+
+# COMMAND ----------
+
+races_df_renamed = races_df_stg.withColumnRenamed('raceid', 'race_id') \
+                               .withColumnRenamed('year', 'race_year') \
+                               .withColumnRenamed('circuitId', 'circuit_id')
+
+races_df_renamed.show(3)                               
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Step 3 & 4**
+# MAGIC - Transform-create a new column to concatenate date and time to create race_timestamp.
+# MAGIC - Add new column called ingestion_date using current timestamp.
+
+# COMMAND ----------
+
+from pyspark.sql.functions import concat, lit, col, to_timestamp, current_timestamp
+
+# COMMAND ----------
+
+# the format for timestamp has a space between the date and time, lit should also be ' ' otherwise null values will show up.
+races_df_transformed = races_df_renamed.withColumn('race_timestamp', to_timestamp(concat(col('date'), lit(' '), col('time')), 'yyyy-MM-dd HH:mm:ss')) \
+                                       .withColumn('ingestion_date', current_timestamp())
+
+# COMMAND ----------
+
+races_df_transformed.show(5, truncate=False)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Step 5**
+# MAGIC - Write the data as a parquet and store it in the clean container.
+
+# COMMAND ----------
+
+# add a partition for race year
+races_df_transformed.write.mode('overwrite').partitionBy('race_year').parquet('/mnt/dlformula1jk/clean/races')
+
+# COMMAND ----------
+
+# MAGIC %md see the files in the container
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/dlformula1jk/clean/races
+
+# COMMAND ----------
+
+# read the parquet file
+display(spark.read.parquet('/mnt/dlformula1jk/clean/races'))
+
+# COMMAND ----------
+
+
